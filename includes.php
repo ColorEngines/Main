d8bed98bfc12e6d81383c935abac5faf018dfd91
afakes <afakes@AFAKES-HP>
2014-01-17 21:07:21 +1100

    Better localisation of libraries

--- includes.php
+++ includes.php
@@ -1,4 +1,9 @@
 <?php
-if (file_exists('utilities/includes.php')) include_once "utilities/includes.php"; 
-if (file_exists('../utilities/includes.php')) include_once "../utilities/includes.php"; 
+GLOBAL $ini;
+$ini = parse_ini_file("ini/".gethostname().'.ini',true);
+
+foreach ($ini['include'] as $value) include_once $value;
+
+foreach (file::folder_with_extension(".", "class", "/") as $filepath) include_once $filepath;     // load all CLASS files
+
 ?>
